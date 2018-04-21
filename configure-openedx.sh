67f7eb3ced59d176bb69dcb515de8012b4603633
Alpha Zhang <hanyi.zhang@outlook.com>
2018-04-21 21:52:19 +0800

    Update to Ubuntu 16.04 and edx-ginkgo.2
    Update to Ubuntu 16.04 and edx-ginkgo.2

--- configure-openedx.sh
+++ configure-openedx.sh
@@ -81,7 +81,7 @@ time sudo pip install --upgrade virtualenv
 ###################################################
 # Pin specific version of Open edX (named-release/cypress for now)
 ###################################################
-export OPENEDX_RELEASE='named-release/cypress'
+export OPENEDX_RELEASE='named-release/ginkgo.2'
 EXTRA_VARS="-e edx_platform_version=$OPENEDX_RELEASE \
   -e certs_version=$OPENEDX_RELEASE \
   -e forum_version=$OPENEDX_RELEASE \
